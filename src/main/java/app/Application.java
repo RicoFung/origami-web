e19f8ad1c2b35e2559b4848ebc5878162c86a83f
RexFun <oleFun@icloud.com>
2017-09-11 13:50:12 +0800

    配置多文件上传包

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,9 +1,13 @@
 package app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
 
-@SpringBootApplication 
+@SpringBootApplication
+//exclude表示自动配置时不包括Multipart配置（使用CommonsMultipartFile实现多文件上传）
+@EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})
 public class Application
 {
     public static void main(String[] args) 
--- src/main/java/chok/util/PropertiesUtil.java
+++ src/main/java/chok/util/PropertiesUtil.java
@@ -6,19 +6,20 @@ import java.util.Properties;
 public class PropertiesUtil
 {
 	private static Properties prop;
-	
+
 	public static String getValue(String key)
 	{
-		if (prop == null) prop = new Properties();
-		try 
-		{  
-		  prop.load(PropertiesUtil.class.getClassLoader().getResourceAsStream("application.properties"));  
-		  return prop.getProperty(key);
-		} 
-		catch(IOException e) 
-		{  
-		  e.printStackTrace();
-		  return "";
-		}  
+		if (prop == null)
+			prop = new Properties();
+		try
+		{
+			prop.load(PropertiesUtil.class.getClassLoader().getResourceAsStream("application.properties"));
+			return prop.getProperty(key);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			return "";
+		}
 	}
 }
